7db84d4802256cdef4f9efc161e3e9d51f077486
Nyavlix <sylvain@chronix>
2016-02-26 16:32:16 +0100

    Add style to template

--- source/app.d
+++ source/app.d
@@ -2,10 +2,14 @@ import vibe.d;
 
 shared static this()
 {
+	auto router = new URLRouter;
+	router.get("/", staticTemplate!"index.dt");
+	router.get("*", serveStaticFiles("public/"));
+
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, staticTemplate!"index.dt");
+	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
