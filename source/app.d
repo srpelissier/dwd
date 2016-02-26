376d1b09b59d60a9f179952d99e5ac7ca2ba2957
Nyavlix <sylvain@chronix>
2016-02-26 16:21:25 +0100

    Start with a simple template

--- source/app.d
+++ source/app.d
@@ -5,12 +5,7 @@ shared static this()
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+	listenHTTP(settings, staticTemplate!"index.dt");
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-}
-
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
 }
